92e13607d0b33bc28a366144579c8c81d06cddcb
Shunn Pyae Pyae Aung <shunnpyaepyaeaung@Shunns-MacBook-Pro.local>
2021-06-25 16:50:30 +0630

    Typography

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -3,10 +3,12 @@ import Nav from './Nav';
 import Footer from './Footer';
 import 'normalize.css';
 import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 const Layout = ({ children }) => (
   <div>
     <GlobalStyles />
+    <Typography />
     <Nav />
     {children}
     <Footer />
